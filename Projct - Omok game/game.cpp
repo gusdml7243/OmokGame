8f30a6c0e1d8af6222770eb5073bcb158f3a8212
image7243 <gusdml7243@naver.com>
2020-06-11 20:32:30 +0900

    초기 커밋.

---
+++ Projct - Omok game/game.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+void topLine(int num);
+void middleLine(int num);
+void bottomLine(int num);
+int main()
+{
+	int const COUNT = 19;
+	cout << " ====== Let's Play Omok ======\n" << endl << " ";
+	for (int i = 1; i <= COUNT; i++)
+		cout << setw(2) << i;
+	cout << endl;
+	topLine(COUNT);
+	for (int i = 2; i < COUNT; i++)
+	{
+		cout << setw(2) << i;
+		middleLine(COUNT);
+	}
+	bottomLine(COUNT);
+	cout << endl << "≒'s X,Y:";
+}
+void topLine(int num)
+{
+	cout << setw(2) << "1" << "忙" << "式";
+	for (int i = 0; i < num - 2; i++)
+	{
+		cout << "成" << "式";
+	}
+	cout << "忖" << endl;
+}
+void middleLine(int num)
+{
+	cout << "戍" << "式";
+	for (int i = 0; i < num - 2; i++)
+	{
+		cout << "托" << "式";
+	}
+	cout << "扣" << endl;
+}
+void bottomLine(int num)
+{
+	cout << setw(2) << num << "戌" << "式";
+	for (int i = 0; i < num - 2; i++)
+	{
+		cout << "扛" << "式";
+	}
+	cout << "戎" << endl;
+}
